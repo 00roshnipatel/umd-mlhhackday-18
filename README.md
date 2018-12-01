9265cd9c76362c9ce1cd22607076c98a958984aa
jasmaa <jasonmaa3955@gmail.com>
2018-12-01 11:36:11 -0500

    Add bg subtraction, fix readme

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# UMD LHD
+# 2D Beatsaber
 
-Repo for local hack day project
-
-<<<<<<< HEAD
-test 3
-=======
-<<<<<<< HEAD
-test commit 2
-=======
-You thought this would be a meaningful message, but it was me TYLER
->>>>>>> 8f6c1df5e35b31448e5cb1cc80586c6607a4ca64
->>>>>>> 4525dd3449fdae557e6cf0260be7c1accb34d47d
+Computer vision beatsaber made for UMD local hack day
--- beatsaber.py
+++ beatsaber.py
@@ -2,15 +2,16 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=25, detectShadows=True)
+
 while cap.isOpened():
     ret, frame = cap.read()
 
-    imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    thresh = fgbg.apply(frame)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     img = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
 
-    cv2.imshow('frame', img)
+    cv2.imshow('frame', cv2.flip(img, 1))
     
     c = cv2.waitKey(1)
     if c == ord('q'):
