c95b28352cbc4f698ea5e537d6987172b713fb8b
jasmaa <jasonmaa3955@gmail.com>
2018-12-01 16:14:25 -0500

    Add animation for bubbles and scoreboard

--- music.py
+++ music.py
@@ -2,8 +2,10 @@ from scipy.io import wavfile
 import pyaudio, wave
 import os
 
+is_playing = True
+
 def play_music(f, data, stream, chunk):
     #play stream
-    while data:
+    while is_playing and data:
         stream.write(data)
         data = f.readframes(chunk)
